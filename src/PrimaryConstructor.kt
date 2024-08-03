0eac10c42d6dce4033e5403faebb7bbc6a0cd2f3
Wafaa Farahat <wafaadarwish76@gmail.com>
2024-08-03 22:12:30 +0300

    create multiple constructor s

--- src/PrimaryConstructor.kt
+++ src/PrimaryConstructor.kt
@@ -1,9 +1,26 @@
 fun main(){
-var person= Invoice(1 , name = "wafaa", total = 175.6)
-}
+//var person= Invoice(1 , name = "wafaa", total = 175.6)
 
+//    var circle1=Circle(name = "wafaa" )
+    var diameter=Circle(dia=12)
+
+}
+// primary constructor
 class Invoice(id:Int ,name:String, total:Double ){
     init {
         println("id :$id , name:$name , total:$total")
+    }
+}
+
+// multiple constructor
+class Circle(radius:Double){
+    constructor(name: String):this(radius = 2.5)
+    constructor(dia:Int ):this(radius = dia/2.0){
+        println("Diameter =  $dia")
+    }
+    init {
+        println("radius = $radius ")
+        println("Area of circle = ${Math.PI * radius * radius}")
+
     }
 }
