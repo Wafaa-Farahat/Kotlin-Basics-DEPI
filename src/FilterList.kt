5b92f644bd6648c9035702bef298924f11a7b62b
Wafaa Farahat <wafaadarwish76@gmail.com>
2024-08-08 22:05:00 +0300

    some refator on files

--- src/FilterList.kt
+++
@@ -1,6 +0,0 @@
-fun main(){
-val names= listOf("Hind","Noura","Asmaa","Hagar")
-    val filterList=names.asSequence().filter {it[0] == 'H'  }
-    println("filtered :${filterList.toList()}")
-}
-
--- src/PrintPassedDayAndMeal.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-   dayAndMeal("saturday","Steak")
-}
-
-fun dayAndMeal(day :String , meal:String){
-    println("The day is $day,The meal is $meal")
-}
-
