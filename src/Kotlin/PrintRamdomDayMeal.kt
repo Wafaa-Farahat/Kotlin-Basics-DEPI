2b282ac5564cf9d20e256b354246707329f82dd5
Wafaa Farahat <wafaadarwish76@gmail.com>
2024-08-08 22:05:43 +0300

    some refator on files

---
+++ src/Kotlin/PrintRamdomDayMeal.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val meals= listOf("Pizza","Burger","Steak","Bread","Chicken")
+    val daysOfWeek = listOf(
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    )
+    for(day in daysOfWeek)
+        randomDayMeal(days=daysOfWeek,meals=meals)
+}
+
+fun randomDayMeal(days: List<String>, meals: List<String>){
+val randomDay=days.random()
+    val randomMeal=meals.random()
+
+    println("The day is :$randomDay,The meal is :$randomMeal")
+}
