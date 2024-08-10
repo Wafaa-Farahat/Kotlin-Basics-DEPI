ebdc2f4e893b7c1fb15b7a76b62b82280127f80a
Wafaa Farahat <wafaadarwish76@gmail.com>
2024-08-10 16:45:08 +0300

    using constructorchaining and method overloading --> this keyword

--- src/Java/Employee.java
+++ src/Java/Employee.java
@@ -1,40 +1,57 @@
 package Java;
 
-public class Employee_Constructor {
-    public static void main(String[] args) {
-        Employee_Constructor e0=new Employee_Constructor();
-        e0.printEmpData();
-        System.out.println("--------------");
-        Employee_Constructor e1= new Employee_Constructor(1,"Ahmed");
-        e1.printEmpData();
-        System.out.println("--------------");
-        Employee_Constructor e2= new Employee_Constructor(2,"Aya",true);
-        e2.printEmpData();
-    }
-
+//using constructor chaining
+public class Employee {
+    //instance fields (used /belong to every instance)
     int emp_id;
-    String name;
+    String ename;
     double salary;
     double bonus;
     String department;
     boolean resident;
 
-    public Employee_Constructor(){}
-    public Employee_Constructor(int id , String n ){
-        name=n;
+//    public Employee(){}
+    public Employee(){
+        emp_id=100;
+        ename="mohamed";
+        salary=8000;
+        bonus=700;
+        department="not assigned yet";
+        resident=true;
+
+    }
+    public Employee(int id , String n ){
+        ename =n;
         emp_id = id;
     }
-    public Employee_Constructor(int empid , String na, boolean r){
-//        emp_id = empid;
-//        name=na;
+    public Employee(int empid , String na, boolean r){
+        //emp_id = empid;
+        //name=na;
+        //calling pre constructor
         this(empid,na);
         resident=r;
     }
+    public Employee(int empid , String na, double s, double b , String d , boolean r){
 
+        this(empid,na,r);
+        salary=s;
+        bonus=b;
+        department=d;
+    }
+
+    public void set_salary(double s){
+        salary=s;
+    }
+    public void set_salary(double s,double b){
+        //this is method, so we can't use this direct like constructor
+        // this(s);
+        this.set_salary(s);
+        bonus=b;
+    }
 
     public void printEmpData(){
         System.out.println("Emp ID" +" "+emp_id);
-        System.out.println("Name" +" "+name);
+        System.out.println("Name" +" "+ ename);
         System.out.println("Salary" +" "+salary);
         System.out.println("Bonus" +" "+ bonus);
         System.out.println("Department" +" "+ department);
